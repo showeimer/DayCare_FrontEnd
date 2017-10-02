17408a1cb0ee4093f50751ee26e7f87aadc70c5a
Brett Colyer <brettc911@gmail.com>
2017-10-02 10:29:22 -0400

    setup file tree and boiler-plate component code in DashboardLayout

---
+++ src/components/DashboardLayout.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+// Imported Styles:
+
+// Imported Images:
+
+// Import Router
+import { Link } from 'react-router-dom';
+
+// STYLES:
+// ________________________________________________
+
+
+
+
+// ________________________________________________
+
+export default class BaseLayout extends Component {
+  render() {
+    return (
+      <div className='container'>
+
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -4,5 +4,15 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+
+  <BrowserRouter>
+    <BaseLayout>
+      <Switch>
+        <Route path="/" component={}/>
+      </Switch>
+    </BaseLayout>
+  </BrowserRouter>
+
+  , document.getElementById('root'));
 registerServiceWorker();
