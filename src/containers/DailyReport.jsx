167813fdcaaa5059f8ab7913f188201f2edb1452
showeimer <showeimer@gmail.com>
2017-10-03 10:35:32 -0400

    created renderField and renderDiaper handlers, successfully adding new diaper change fields on button click

--- src/containers/DailyReport.jsx
+++ src/containers/DailyReport.jsx
@@ -1,12 +1,79 @@
 import React, { Component } from 'react';
 import { Field, FieldArray, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class DailyReport extends Component {
-  render() {
+
+  renderField = ({ input, label, type, meta: { touched, error } }) => {
     return (
       <div>
-        <h1>Daily Report Component</h1>
+        <label>{label}</label>
+        <div>
+          <input {...input} type={type} placeholder={label} />
+          {touched && error && <span> {error} </span>}
+        </div>
       </div>
+    );
+  };
+
+
+  renderDiaper = ({fields, meta: { error } }) => {
+    return (
+      <ul>
+        <li>
+          <button type="button" onClick={() => fields.push({})}>Add Diaper Change</button>
+        </li>
+
+        {fields.map((diaper, index) => {
+          return (
+            <li key={index}>
+              <button type="button" title="Remove Diaper Change" onClick={() => fields.remove(index)}>X</button>
+
+              <Field
+                name={`${diaper}.time`}
+                type="text"
+                component={this.renderField}
+                label="Time"
+              />
+              <label>
+                <Field
+                  name={`${diaper}.type`}
+                  component={this.renderField}
+                  type="radio"
+                  value="wet"
+                />
+                Wet
+              </label>
+              <label>
+                <Field
+                  name={`${diaper}.type`}
+                  component={this.renderField}
+                  type="radio"
+                  value="bm"
+                />
+                BM
+              </label>
+            </li>
+          )
+        })}
+      </ul>
+    )
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit}>
+        <h1>{this.props.name}'s Daily Report'</h1>
+        <Link to="/dashboard">Back</Link>
+
+        <fieldset>
+          <legend>Diaper</legend>
+          <FieldArray name="diaper" component={this.renderDiaper} />
+        </fieldset>
+        <button>Save</button>
+      </form>
     );
   }
 }
