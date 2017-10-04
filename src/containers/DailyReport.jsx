0f494c1590c707cb1557c4eacd80e923eca5c759
showeimer <showeimer@gmail.com>
2017-10-03 20:57:16 -0400

    validation working for daily report input fields if user does not enter text, displays error

--- src/containers/DailyReport.jsx
+++ src/containers/DailyReport.jsx
@@ -49,7 +49,7 @@ class DailyReport extends Component {
               <button type="button" title="Remove Meal" onClick={() => fields.remove(index)}>X</button>
 
               <label>
-                <Field name={`${meal}.type`} component="select">
+                <Field name={`${meal}.type`} component='select'>
                   <option />
                   <option value="breakfast">Breakfast</option>
                   <option value="lunch">Lunch</option>
@@ -268,7 +268,7 @@ function validate(values) {
   const errors = {};
 
   if(!values.meals) {
-    console.log('nothing');
+    console.log('No meals errors');
   } else {
     const mealsArrayErrors = [];
     values.meals.forEach((meal, mealIndex) => {
@@ -277,10 +277,59 @@ function validate(values) {
         mealErrors.food = 'Required';
         mealsArrayErrors[mealIndex] = mealErrors;
       }
+
+      if (!meal || !meal.type) {
+        mealErrors.type = 'Required';
+        mealsArrayErrors[mealIndex] = mealErrors;
+      }
     });
 
     if(mealsArrayErrors.length) {
       errors.meals = mealsArrayErrors;
+    }
+  }
+
+  if(!values.diapers) {
+    console.log('No diapers errors');
+  } else {
+    const diapersArrayErrors = [];
+    values.diapers.forEach((diaper, diaperIndex) => {
+      const diaperErrors = {};
+      if(!diaper || !diaper.time) {
+        diaperErrors.time = 'Required';
+        diapersArrayErrors[diaperIndex] = diaperErrors;
+      }
+
+      if (!diaper || !diaper.type) {
+        diaperErrors.type = 'Required';
+        diapersArrayErrors[diaperIndex] = diaperErrors;
+      }
+    });
+
+    if(diapersArrayErrors.length) {
+      errors.diapers = diapersArrayErrors;
+    }
+  }
+
+  if(!values.naps) {
+    console.log('nothing');
+  } else {
+    const napsArrayErrors = [];
+    values.naps.forEach((nap, napIndex) => {
+      const napErrors = {};
+      if(!nap || !nap.napStart) {
+        napErrors.napStart = 'Required';
+        napsArrayErrors[napIndex] = napErrors;
+      }
+
+      if (!nap || !nap.napEnd) {
+        napErrors.napEnd = 'Required';
+        napsArrayErrors[napIndex] = napErrors;
+      }
+    });
+
+    if(napsArrayErrors.length) {
+      errors.naps = napsArrayErrors;
     }
   }
 
