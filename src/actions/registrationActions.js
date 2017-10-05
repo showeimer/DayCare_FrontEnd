9fe8894c5e245bce5b48b30864f5ce33f1cae775
showeimer <showeimer@gmail.com>
2017-10-05 14:42:43 -0400

    fetch request handling in registerDaycare action creator

--- src/actions/registrationActions.js
+++ src/actions/registrationActions.js
@@ -1,10 +1,19 @@
 import { REGISTER_DAYCARE } from './types';
 
-export const registerDaycare = values => {
-  console.log(values);
-
-  return {
-    type: REGISTER_DAYCARE,
-    payload: values
+export const registerDaycare = () => {
+  return (dispatch) => {
+      fetch('https://demo8413433.mockable.io/daycares/register', {
+        method: 'POST'
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        return dispatch ({
+          type: REGISTER_DAYCARE,
+          payload: null
+        })
+      })
   }
+
+
 }
