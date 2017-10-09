bb7527f63c7cc3ebb3b4091d586752ff02af0da5
Brett Colyer <brettc911@gmail.com>
2017-10-09 17:31:41 -0400

    made a few style changes to registration form

--- src/containers/Registration.js
+++ src/containers/Registration.js
@@ -3,6 +3,31 @@ import { connect } from 'react-redux';
 import { Field, FieldArray, reduxForm } from 'redux-form';
 import { renderField } from './report'
 import { registerDaycare } from '../actions';
+import styled from 'styled-components'
+
+const H2 = styled.h2`
+  color: #fff;
+  font-size: 25px;
+  text-align: center;
+  padding: 30px 0;
+`
+const H1 = styled.h1`
+margin-top: 20px;
+color: #fff;
+text-align: center;
+font-size: 40px;
+font-family: 'Lobster Two', cursive;
+&:after ${H1}{
+  content: "";
+  display: block;
+  width: 38px;
+  border: solid 2px #50E3C2;
+  margin: auto;
+}
+`
+const Form = styled.form`
+  font-size: 18px;
+`
 
 class RegistrationPage extends Component {
 
@@ -16,9 +41,11 @@ class RegistrationPage extends Component {
     const { handleSubmit, pristine, submitting } = this.props;
 
     return (
-      <div>
-        <form onSubmit={handleSubmit(this.onSubmit.bind(this))} className='primary-form'>
-          <label>What is the name of your daycare?</label>
+      <div className='container'>
+        <H1>inForm.</H1>
+        <H2>Register your Daycare</H2>
+        <Form onSubmit={handleSubmit(this.onSubmit.bind(this))} className='primary-form'>
+          <label>Daycare Name</label>
           <Field
             name="name"
             type="text"
@@ -71,7 +98,7 @@ class RegistrationPage extends Component {
               Register
             </button>
           </div>
-        </form>
+        </Form>
       </div>
     )
   }
